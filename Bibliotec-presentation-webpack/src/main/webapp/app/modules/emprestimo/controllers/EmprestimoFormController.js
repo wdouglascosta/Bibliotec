6bb7ed297690bdace18ef8743d1b7992ecad7ae0
wdouglascosta <wdouglascosta@github.com>
2017-11-14 07:40:19 -0200

    alterações

--- Bibliotec-presentation-webpack/src/main/webapp/app/modules/emprestimo/controllers/EmprestimoFormController.js
+++ Bibliotec-presentation-webpack/src/main/webapp/app/modules/emprestimo/controllers/EmprestimoFormController.js
@@ -8,7 +8,6 @@ function EmprestimoFormController(EmprestimoService, $state, entity, $scope, gum
 	gumgaController.createRestMethods($scope, UsuarioService, 'usuario');
 	gumgaController.createRestMethods($scope, ItemService, 'item');
     $scope.item.execute('get')
-	console.log($scope.item)
 	$scope.itemSelecionado = null;
     $scope.usuarioSelecionado = null;
 	$scope.usuario.methods.search('nome','');
@@ -36,32 +35,12 @@ function EmprestimoFormController(EmprestimoService, $state, entity, $scope, gum
 
 	$scope.emprestimo.on('putSuccess',function(data){
 		$state.go('emprestimo.list');
+		console.log(emprestimo)
 	});
 
 	//-----------------------------------------------------------------------------
 
-            $scope.produtos = [
-                { nome: 'Notebook Acer Aspire', id: 1 },
-                { nome: 'Motorola Moto X (2a Geração) 32GB', id: 2 },
-                { nome: 'Smart TV LED 43" Samsung', id: 3 },
-                { nome: 'Ar Condicionado Split 7000 BTU/s', id: 4 }
-            ];
 
-            $scope.produto = $scope.produtos[1];
-
-            $scope.deselectCallback = function(value){
-                console.log(value);
-            }
-
-            // Este método precisa ser assíncrono
-            $scope.getSearch = function(param){
-                return $q(function(resolve){
-                    var arr = $scope.produtos.filter(function(produto){
-                        return produto.nome.indexOf(param) != -1;
-                    })
-                    resolve(arr);
-                })
-            };
 	//-----------------------------------------------------------------------------
 
 	$scope.metodoTeste = function () {
