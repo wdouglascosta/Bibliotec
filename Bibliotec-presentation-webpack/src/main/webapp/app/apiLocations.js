c3c001b3c932592f33f2567054cc911f27249c43
MateusHAS <ra90253@uem.br>
2017-11-17 00:25:00 -0200

    nova branch

--- Bibliotec-presentation-webpack/src/main/webapp/app/apiLocations.js
+++ Bibliotec-presentation-webpack/src/main/webapp/app/apiLocations.js
@@ -1,5 +1,5 @@
 'use strict';
 window.APILocation = {
     // apiLocation: location.origin+"/Bibliotec-api"
-    apiLocation: "http://localhost:8080/Bibliotec-api"
+    apiLocation: "http://localhost:8090/Bibliotec-api"
 };
